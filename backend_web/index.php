b68a10a220a4b465095edf7fc0304fac3ad3ac60
ioedumac <eacevedof@yahoo.es>
2020-11-17 11:05:13 +0100

    reseteando index.php para rebase

--- backend_web/index.php
+++ backend_web/index.php
@@ -1,12 +1,2 @@
 <?php
-echo "
-Esta es mi rama maestra
-
-Develop modificado en feature
-
-Linea de feature-uno-1
-quito lineas y este cambio sube a develop
-
-Aqui termina
-Fin feature
-";
+echo "";
